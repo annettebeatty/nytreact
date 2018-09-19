6cbe032f82024f8720b08396133ac7d07f86726d
Beatty, Annette <Annette_Beatty@intuit.com>
2018-09-18 23:24:04 -0700

    Took out unused card

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card } from "reactstrap";
 import "./styles/Saved.css";
 
 const Saved = () => (
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card } from "reactstrap";
 import "./styles/Search.css";
 
 const Search = () => (
