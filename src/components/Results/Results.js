cab351d1e706179c3b43d25fa6e7d433afc9d371
Beatty, Annette <Annette_Beatty@intuit.com>
2018-09-23 22:36:42 -0700

    Formatted Results section

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -8,9 +8,7 @@ const ResultsList = props => (
     {
         props.results.map(result => (
             <li className="list-group-item" key={result._id}>
-                {result.headline.main}
-                {result.web_url}
-                {result.pub_date}
+            <a href={result.web_url} target="_blank">{result.headline.main}</a>
             </li>
         ))
     }
