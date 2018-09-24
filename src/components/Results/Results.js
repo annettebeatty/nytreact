f7e24bfeb718bc37f1ed999ff7dd43ac23932bc8
Beatty, Annette <Annette_Beatty@intuit.com>
2018-09-23 22:48:06 -0700

    Added byline

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Results.css";
 
+function renderByline(result){
+    if (result.byline) {
+      return result.byline.original;
+    }
+};
+
 const ResultsList = props => (
     <div className="card card-header">
         <h3 className="card-title rounded-top"><strong><i className="fa fa-table"></i> Results</strong></h3>
@@ -8,7 +14,8 @@ const ResultsList = props => (
     {
         props.results.map(result => (
             <li className="list-group-item" key={result._id}>
-            <a href={result.web_url} target="_blank">{result.headline.main}</a>
+            <h5><a href={result.web_url} target="_blank">{result.headline.main}</a></h5>
+            <h6>{renderByline(result)}</h6>
             </li>
         ))
     }
